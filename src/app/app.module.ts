2c6a40dbd5abdde03cedaa380dd638187d222c20
tereza-tech <tereza.tech@gmail.com>
2022-11-26 12:27:32 +0000

    prefork-ng-supa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { AuthComponent } from './components/auth/auth.component'
+import { HomeComponent } from './components/home/home.component'
+import { AppRoutingModule } from './app-routing.module'
+import { TodoItemComponent } from './components/todo-item/todo-item.component'
+import { RecoveryPasswordComponent } from './components/recovery-password/recovery-password.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    TodoItemComponent,
+    RecoveryPasswordComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
